c0cd3eb6a08aa53d18ec5cc3a59c13fbdfa95ae3
Harsh Raj <harsh.raj.phe16@itbhu.ac.in>
2025-07-31 15:26:31 +0530

    basic implementations

--- src/main/java/org/example/Model/Board.java
+++ src/main/java/org/example/Model/Board.java
@@ -1,8 +1,40 @@
 package org.example.Model;
 
+import java.util.List;
 
-public interface Board {
-    void display();
-    void reset();
-    boolean isValidPosition(Cell cell);
+public class Board{
+    private int size;
+    private List<List<Cell>> board;
+
+    public Board(int dimension) {
+
+    }
+
+    //Getter&Setter
+    public void setBoard(List<List<Cell>> board) {
+        this.board = board;
+    }
+    public void setSize(int size) {
+        this.size = size;
+    }
+    public List<List<Cell>> getBoard() {
+        return board;
+    }
+    public int getSize() {
+        return size;
+    }
+
+    //Methods
+
+    public void display() {}
+
+
+    public void reset() {
+
+    }
+
+
+    public boolean isValidPosition(Cell cell) {
+        return false;
+    }
 }
